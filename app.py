81a6a23f622465b5229d1530750d5b6d65584130
RoelandKramer <r.s.kramer_1@tilburguniversity.edu>
2026-01-22 16:10:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ st.sidebar.markdown(
     }}
     .sidebar-logo {{
         position: absolute;
-        top: 240px;
+        top: 180px;
         left: 50%;
         transform: translateX(-50%);
         width: 160px;
@@ -168,6 +168,7 @@ if run:
         st.error(str(e))
 else:
     st.info("Pick a player and click **Generate radar chart**.")
+
 
 
 
